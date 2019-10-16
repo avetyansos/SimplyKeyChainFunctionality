b65bada6eecd8cfbb3dc56cefcc9995b074de6cd
Sos Avetyan <avetyansos@yahoo.com>
2019-10-16 09:08:05 +0300

    Initial Commit

---
+++ Save Items To KeyChain/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Save Items To KeyChain
+//
+//  Created by Sos Avetyan on 16/10/2019.
+//  Copyright © 2019 Sos Avetyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Save Items To KeyChainTests/Save_Items_To_KeyChainTests.swift
@@ -0,0 +1,34 @@
+//
+//  Save_Items_To_KeyChainTests.swift
+//  Save Items To KeyChainTests
+//
+//  Created by Sos Avetyan on 16/10/2019.
+//  Copyright © 2019 Sos Avetyan. All rights reserved.
+//
+
+import XCTest
+@testable import Save_Items_To_KeyChain
+
+class Save_Items_To_KeyChainTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
